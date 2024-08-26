68ac53dd111bb55888c056236f991fc1e517c4fb
poornima0102 <141801145+poornima0102@users.noreply.github.com>
2024-08-26 12:13:22 +0530

    Time: 0 ms (100%), Space: 7.6 MB (12.61%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int bottomUp(int n){
-        vector<int>dp(n+1,-1);
+    vector<int>dp(n+1,-1);
+      if(n == 0){
+            return 0;
+        }
+        else if((n == 1) || (n==2)){
+            return 1;
+        }
         dp[0]=0;
-        if(n==0) return dp[0];
-        dp[1]=1;
-        if(n==1) return dp[1];
-        dp[2]=1;
-        if(n==2) return dp[2];
+        dp[1] = 1;
+        dp[2] = 1; 
+
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1];
-            if(i-2>=0)
-            dp[i]+=dp[i-2];
-            if(i-3>=0)
-            dp[i]+=dp[i-3];
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
         return dp[n];
 
