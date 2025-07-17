145b4227d4473b4513baaeca7ddb1131620c550c
poornima0102 <141801145+poornima0102@users.noreply.github.com>
2025-07-17 22:09:57 +0530

    Time: 0 ms (100%), Space: 26.1 MB (23.99%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-    
-        // Initialize two pointers: even index and odd index
-        int e = 0, o = 1;
-
-        // Loop until either pointer exceeds the array size
-        while(e < nums.size() && o < nums.size()) {
-
-            // Move even pointer forward if the number is already even
-            while(e < nums.size() && nums[e] % 2 == 0) {
-                e += 2;
-            }
-
-            // Move odd pointer forward if the number is already odd
-            while(o < nums.size() && nums[o] % 2 == 1) {
-                o += 2;
-            }
-
-            // Swap if both pointers point to incorrect placements
-            if(e < nums.size() && o < nums.size()) {
-                swap(nums[o], nums[e]);
-            }
+    int e=0,o=1;
+    while(e<nums.size() && o<nums.size()){
+        while(e<nums.size() && nums[e]%2==0){
+            e+=2;
         }
-
-        // Return the corrected array
-        return nums;
+        while(o<nums.size() && nums[o]%2!=0){
+            o+=2;
+        }
+        if(e < nums.size() && o < nums.size()) {
+            swap(nums[o], nums[e]);
+        }
+    }
+     return nums;   
     }
 };
